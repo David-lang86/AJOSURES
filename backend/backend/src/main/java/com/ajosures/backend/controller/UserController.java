30157eb180985b194eec1770c057a90578e7dac7
Torres0407 <okuborchinonye@gmail.com>
2026-06-01 10:30:10 +0100

    frontendtest2

--- backend/backend/src/main/java/com/ajosures/backend/controller/UserController.java
+++ backend/backend/src/main/java/com/ajosures/backend/controller/UserController.java
@@ -3,6 +3,7 @@ package com.ajosures.backend.controller;
 import com.ajosures.backend.model.User;
 import com.ajosures.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,8 +14,12 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     @PostMapping("/register")
     public User register(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
